a76f408473d2a6ab915ab95ffc657bb8e956936a
vince-0202 <w.vince.0202@gmail.com>
2022-05-09 10:06:50 +0800

    add some slice func

--- pkg/basic/sliceutil/slice.go
+++ pkg/basic/sliceutil/slice.go
@@ -1,15 +1,32 @@
 package sliceutil
 
 //IsEmpty Check if a slice is empty
-func IsEmpty[T int8 | int | int16 | int32 | int64 | string | float32 | float64 | bool | byte](s []T) bool {
+func IsEmpty[T any](s []T) bool {
 	if nil == s {
 		return true
 	}
 	return len(s) == 0
 }
 
+//IsNotEmpty Check if a slice is not empty
+func IsNotEmpty[T any](s []T) bool {
+	return !IsEmpty(s)
+}
+
+//HasNull Check if has null in slices
+func HasNull[T any](s ...[]T) bool {
+	if IsNotEmpty(s) {
+		for _, v := range s {
+			if nil == v {
+				return true
+			}
+		}
+	}
+	return false
+}
+
 //Insert Insert a element or a slience at the specified position of the sli
-func Insert[T int8 | int | int16 | int32 | int64 | string | float32 | float64 | bool | byte](sli []T, i int, element ...T) []T {
+func Insert[T any](sli []T, i int, element ...T) []T {
 
 	if IsEmpty(element) {
 		return sli
@@ -28,6 +45,14 @@ func Insert[T int8 | int | int16 | int32 | int64 | string | float32 | float64 | 
 	return sli
 }
 
-// func AddAll[T int8 | int | int16 | int32 | int64 | string | float32 | float64 | bool | byte](slis ...[]T) []T {
+func Delete[T any](sli []T, start, end int) (res []T) {
 
-// }
+	if IsEmpty(sli) || end >= len(sli) || start > end || start < 0 {
+		return sli
+	}
+
+	res = append(res, sli[:start]...)
+	res = append(res, sli[end+1:]...)
+
+	return
+}
--- pkg/basic/sliceutil/slice_test.go
+++ pkg/basic/sliceutil/slice_test.go
@@ -5,9 +5,14 @@ import (
 	"testing"
 )
 
+type Dog struct {
+	kind string
+	name string
+}
+
 func TestInster(t *testing.T) {
 
-	type args[T int | string] struct {
+	type args[T any] struct {
 		oldSlice []T
 		i        int
 		newSlice []T
@@ -76,6 +81,22 @@ func TestInster(t *testing.T) {
 		},
 	}
 
+	type stuTest struct {
+		name string
+		args args[Dog]
+		want []Dog
+	}
+
+	stu_test := []stuTest{{
+		name: "hello",
+		args: args[Dog]{
+			oldSlice: []Dog{Dog{name: "胖虎", kind: "柯基"}, Dog{name: "hi", kind: "tugou"}},
+			i:        0,
+			newSlice: []Dog{Dog{name: "add", kind: "test"}},
+		},
+		want: []Dog{Dog{name: "add", kind: "test"}, Dog{name: "胖虎", kind: "柯基"}, Dog{name: "hi", kind: "tugou"}},
+	}}
+
 	for _, tt := range int_tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := Insert(tt.args.oldSlice, tt.args.i, tt.args.newSlice...); !reflect.DeepEqual(got, tt.want) {
@@ -88,6 +109,45 @@ func TestInster(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := Insert(tt.args.oldSlice, tt.args.i, tt.args.newSlice...); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Inster() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+
+	for _, tt := range stu_test {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Insert(tt.args.oldSlice, tt.args.i, tt.args.newSlice...); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Inster() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+
+}
+
+func TestDelete(t *testing.T) {
+	type stringArgs struct {
+		sli   []string
+		start int
+		end   int
+	}
+	tests := []struct {
+		name string
+		args stringArgs
+		want []string
+	}{
+
+		{name: "normal case", args: stringArgs{sli: []string{"zero", "one", "two", "three", "fore"}, start: 1, end: 3}, want: []string{"zero", "fore"}},
+		{name: "normal case", args: stringArgs{sli: []string{"zero", "one", "two", "three", "fore"}, start: 0, end: 4}, want: nil},
+		{name: "normal case", args: stringArgs{sli: []string{"zero", "one", "two", "three", "fore"}, start: 0, end: 0}, want: []string{"one", "two", "three", "fore"}},
+		{name: "normal case", args: stringArgs{sli: []string{"zero", "one", "two", "three", "fore"}, start: 4, end: 4}, want: []string{"zero", "one", "two", "three"}},
+		{name: "normal case", args: stringArgs{sli: []string{"zero", "one", "two", "three", "fore"}, start: 4, end: 3}, want: []string{"zero", "one", "two", "three", "fore"}},
+		{name: "normal case", args: stringArgs{sli: []string{"zero", "one", "two", "three", "fore"}, start: 1, end: 5}, want: []string{"zero", "one", "two", "three", "fore"}},
+		{name: "normal case", args: stringArgs{sli: nil, start: 1, end: 3}, want: nil},
+		{name: "normal case", args: stringArgs{sli: []string{}, start: 1, end: 3}, want: []string{}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Delete(tt.args.sli, tt.args.start, tt.args.end); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Delete() = %v, want %v", got, tt.want)
 			}
 		})
 	}
