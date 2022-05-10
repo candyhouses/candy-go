6d1db6d992806b0d9de8854bfe20969f768721b0
vince-0202 <w.vince.0202@gmail.com>
2022-05-10 18:47:38 +0800

    add range and filter func for sliceutil

--- pkg/basic/sliceutil/slice.go
+++ pkg/basic/sliceutil/slice.go
@@ -56,3 +56,53 @@ func Delete[T any](sli []T, start, end int) (res []T) {
 
 	return
 }
+
+func DeleteByIndex[T any](sli []T, i int) []T {
+
+	return Delete(sli, i, i)
+}
+
+//------ Range
+
+func RangeByStep(start, end, step int) []int {
+	if start > end {
+		start, end = end, start
+	}
+
+	if step <= 0 {
+		step = 1
+	}
+
+	deviation := end - start
+	length := deviation / step
+
+	if deviation%step != 0 {
+		length += 1
+	}
+
+	sli := make([]int, length)
+
+	for i := 0; i < length; i++ {
+		sli[i] = start
+		start += step
+	}
+	return sli
+}
+
+func Range(end int) []int {
+	return RangeByStep(0, end, 1)
+}
+
+//-------filter
+
+func Filter[T any](sli []T, filter func(element T) bool) (res []T) {
+
+	for _, v := range sli {
+
+		if !filter(v) {
+			res = append(res, v)
+		}
+	}
+
+	return
+}
